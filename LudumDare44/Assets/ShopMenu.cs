63bc7e9f58e7ec2f2f45da12cda29271b0ecdbbf
Cody Nguyen <cody.nguyen.films@gmail.com>
2019-04-28 02:16:40 -0500

    95% done

---
+++ LudumDare44/Assets/ShopMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMenu : MonoBehaviour
+
+
+{
+
+    public GameObject endMenu;
+    public GameObject shop;
+
+
+    public void openEndMenu()
+    {
+        shop.SetActive(false);
+
+        endMenu.SetActive(true);
+    }
+
+
+    public void OpenShop()
+
+    {
+        shop.SetActive(true);
+
+        endMenu.SetActive(false);
+
+    }
+}
